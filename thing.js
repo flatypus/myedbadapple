df81c641c640883ebeb319e3f1565494f89fefd7
flatypus <yhc3141@gmail.com>
2022-12-03 23:02:10 -0800

    added song and basic playing

--- thing.js
+++ thing.js
@@ -1,13 +1,12 @@
 // get the element itself, since that's our entrypoint
-listGridFixed = document.getElementsByClassName("listGridFixed")[0];
+list_grid_fixed = document.getElementsByClassName("listGridFixed")[0];
 // certified BC education html moment
 table =
-  listGridFixed.children[0].children[0].children[0].children[0].children[0]
+  list_grid_fixed.children[0].children[0].children[0].children[0].children[0]
     .children[0];
 // table is now a list of trs
 // we only edit the second row and below, and second column and after that
 // convert table.children into an array using Array.from()
-
 let elems = [];
 Array.from(table.children).forEach((row, yindex) => {
   if (yindex > 0) {
@@ -21,8 +20,24 @@ Array.from(table.children).forEach((row, yindex) => {
   }
 });
 console.log(elems);
-// now we have a list of 9x8=72 elements
-let binary_data = fetch(
-  "https://github.com/flatypus/myedbadapple/blob/master/binary.txt?raw=true"
-).then((res) => JSON.parse(res.text()));
-print(binary_data);
+// now we have a list of 8x9=72 elements
+let url =
+  "https://raw.githubusercontent.com/flatypus/myedbadapple/master/binary.txt";
+let response = await fetch(url);
+let data = await response.text();
+let binary_data = JSON.parse(data);
+// console.log(binary_data);
+let frame = 0;
+setInterval(() => {
+  for (let y = 0; y < 9; y++) {
+    for (let x = 0; x < 8; x++) {
+      //   console.log(binary_data[frame][y][x], elems[y][x], x, y);
+      if (binary_data[frame][y][x] == 1) {
+        elems[y][x].style.backgroundColor = "black";
+      } else {
+        elems[y][x].style.backgroundColor = "white";
+      }
+    }
+  }
+  frame++;
+}, 1000 / 30);
