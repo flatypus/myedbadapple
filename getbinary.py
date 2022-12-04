ba76f327dfb76e5789b89547d910378f0245160d
flatypus <yhc3141@gmail.com>
2022-12-03 22:45:13 -0800

    json.dumps lmfao

---
+++ getbinary.py
@@ -0,0 +1,33 @@
+import numpy as np
+import cv2
+import skimage.exposure
+import json
+
+# read mp4 file
+vidcap = cv2.VideoCapture('badapple.mp4')
+success, image = vidcap.read()
+
+count = 0
+all_frames = []
+# pixels of the output
+XOUTPUT, YOUTPUT = 18, 16
+
+while success:
+    success, image = vidcap.read()
+    if (count % 3 == 0):
+        blur = cv2.GaussianBlur(image, (0, 0), sigmaX=3,
+                                sigmaY=3, borderType=cv2.BORDER_DEFAULT)
+        result = skimage.exposure.rescale_intensity(
+            blur, in_range=(127.5, 255), out_range=(0, 255))
+        grayscale = cv2.cvtColor(result.astype(np.uint8), cv2.COLOR_BGR2GRAY)
+        shrunk = cv2.resize(grayscale, (XOUTPUT, YOUTPUT))
+        ret, binary = cv2.threshold(shrunk, 127, 1, cv2.THRESH_BINARY)
+        # Image.fromarray(shrunk).save(f"binary/{count}.png")
+        binary = [i.tolist() for i in binary]
+        all_frames.append(binary)
+        # print(binary)
+        print("Frame", count)
+    count += 1
+
+
+open("binary.txt", "w").write(json.dumps(all_frames))
--- thing.js
+++ thing.js
@@ -1,1 +1,23 @@
-console.log("hello!");
+// get the element itself, since that's our entrypoint
+listGridFixed = document.getElementsByClassName("listGridFixed")[0];
+// certified BC education html moment
+table =
+  listGridFixed.children[0].children[0].children[0].children[0].children[0]
+    .children[0];
+// table is now a list of trs
+// we only edit the second row and below, and second column and after that
+// convert table.children into an array using Array.from()
+let elems = [];
+Array.from(table.children).forEach((row, yindex) => {
+  if (yindex > 0) {
+    rowlist = [];
+    Array.from(row.children).forEach((col, xindex) => {
+      if (xindex > 0) {
+        rowlist.push(col);
+      }
+    });
+    elems.push(rowlist);
+  }
+});
+console.log(elems);
+// now we have a list of 9x8=72 elements
